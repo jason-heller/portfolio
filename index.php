241b0cce6dbc458a13cbd10aac652d946fb8c31c
jheller9 <jasonheller96@gmail.com>
2021-06-15 21:25:59 -0400

    php header

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
 <?php include_once("home.html"); ?>
-<?php include("header.html"); ?>
+<?php include_once("about.html"); ?>
+<?php include_once("contact.html"); ?>
+<?php include_once("projects.html"); ?>
+<?php include_once("header.html"); ?>
